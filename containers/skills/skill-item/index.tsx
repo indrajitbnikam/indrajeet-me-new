367311872c2560573ee8a7b06d4814d527e3a141
indrajitbnikam <indrajitbnikam@gmail.com>
2022-01-01 23:25:14 +0530

    style: optimise skills grid

--- containers/skills/skill-item/index.tsx
+++ containers/skills/skill-item/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const SkillItem = ({ iconSrc, name, description }: any) => {
   return (
-    <div className="flex align-middle border rounded-md border-transparent px-6 py-4 bg-[#fffdf8b0] dark:bg-[#fffdf81a] sm:hover:scale-105 transition-transform">
-      <div className="mr-4 flex align-middle">
+    <div className="flex flex-col items-center sm:justify-start justify-center sm:flex-row sm:align-middle border rounded-md border-transparent px-6 py-4 bg-[#fffdf8b0] dark:bg-[#fffdf81a] sm:hover:scale-105 transition-transform">
+      <div className="mb-1 sm:mr-4 flex align-middle">
         <Image alt="name" src={iconSrc} width={32} height={32} />
       </div>
       <p className="flex align-middle text-xl m-0 p-0 text-gray-600 dark:text-gray-300">
